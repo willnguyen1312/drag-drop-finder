07a144ca77f57f0a21123516b100a3cb325ace31
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-05-17 09:52:27 +0700

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
+  mode: "jit",
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    // classes that are generated dynamically, e.g. `rounded-${size}` and must
-    // be kept
+    enabled: process.env.NODE_ENV === "production",
     safeList: [],
-    content: [
-      './index.html',
-      './src/**/*.vue',
-      './src/**/*.js',
-      // etc.
-    ],
+    content: ["./index.html", "./src/**/*.vue", "./src/**/*.js"],
   },
-  theme: {
-    extend: {
-      fontWeight: ['hover', 'focus'],
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-}
+};
